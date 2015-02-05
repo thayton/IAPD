2f346339a825556ae63fce2c4af71017900e85fe
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-05 10:56:51 -0500

    dont make legal_name unique

--- scraper/custom_scraper/models.py
+++ scraper/custom_scraper/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class IapdFirm(models.Model):
     query_name = models.CharField(max_length=64)
-    legal_name = models.CharField(max_length=64)
-    other_name = models.CharField(max_length=64)
+    legal_name = models.CharField(max_length=64, blank=True)
+    other_name = models.CharField(max_length=64, blank=True)
 
-    sec_number = models.CharField(max_length=12)
+    sec_number = models.CharField(max_length=12, blank=True)
 
-    address = models.TextField()
+    address = models.TextField(blank=True)
     checked = models.BooleanField(default=False)
 
     def __str__(self):
