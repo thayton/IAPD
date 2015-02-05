666b46fda8764dfd61cd927110c18dbd15633981
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-05 10:21:48 -0500

    pull from excel

--- scraper.py
+++ scraper.py
@@ -2,9 +2,28 @@
 
 import re
 import mechanize
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
+from xlrd import open_workbook
+
+XLSX_FILE = 'Legal Names.xlsx'
 IAPD_URL = 'http://www.adviserinfo.sec.gov/IAPD/Content/Search/iapd_Search.aspx'
+
+def legal_names(file=XLSX_FILE):
+    '''
+    Read in legal names from the excel spreadsheet with the assumption
+    that names to be searched for are in column 1. Skip row 1 header
+    '''
+    wb = open_workbook(file)
+    s = wb.sheets()[0]
+
+    col = 1
+    for row in range(s.nrows)[1:]:
+        name = s.cell(row, col).value
+        if type(name) == int:
+            continue
+
+        yield name
 
 class IapdScraper(object):
     def __init__(self):
@@ -12,7 +31,11 @@ class IapdScraper(object):
         self.br.addheaders = [('User-agent', 
                                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
-    def scrape(self, q='1919 INVESTMENT COUNSEL, LLC'):
+
+    def scrape(self, q):
+        '''
+        Search for a Firm in the IAPD database by name
+        '''
         def select_form(form):
             return form.attrs.get('id', None) == 'aspnetForm'
 
@@ -29,6 +52,10 @@ class IapdScraper(object):
         r = re.compile(r'^ctl\d+_cphMainContent_grOrgResults$')
         t = s.find('table', id=r)
 
+        if not t:
+            print 'Not Found'
+            return # No results
+
         tr = t.findAll('tr', recursive=False)
         tr = tr[2:-1] # Skip records-per-page header/footer and title header
 
@@ -42,4 +69,7 @@ class IapdScraper(object):
 
 if __name__ == '__main__':
     scraper = IapdScraper()
-    scraper.scrape()
+
+    for name in legal_names():
+        print '\nq=%s' % name
+        scraper.scrape(q=name)
