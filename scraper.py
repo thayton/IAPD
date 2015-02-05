824cf4fd8d1f407f955ddb242e96ba5c7e775ff1
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-05 10:56:34 -0500

    django backend

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+import os
 import re
+import sys
+import django
 import mechanize
 
 from bs4 import BeautifulSoup
@@ -8,6 +11,17 @@ from xlrd import open_workbook
 
 XLSX_FILE = 'Legal Names.xlsx'
 IAPD_URL = 'http://www.adviserinfo.sec.gov/IAPD/Content/Search/iapd_Search.aspx'
+
+#------------------------------------------------------------------------------------------
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), 'scraper/')))
+sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), 'scraper/scraper/')))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+django.setup()
+
+from django.core.exceptions import ObjectDoesNotExist
+from custom_scraper.models import *
+#------------------------------------------------------------------------------------------
 
 def legal_names(file=XLSX_FILE):
     '''
@@ -39,33 +53,46 @@ class IapdScraper(object):
         def select_form(form):
             return form.attrs.get('id', None) == 'aspnetForm'
 
+        try:
+            firm = IapdFirm.objects.get(query_name=q)
+        except ObjectDoesNotExist:
+            firm = IapdFirm(query_name=q)
+            firm.save()
+        else:
+            if firm.checked:
+                return
+
         self.br.open(IAPD_URL)
         self.br.select_form(predicate=select_form)
         self.br.form['ctl00$cphMainContent$ucUnifiedSearch$rdoSearchBy'] = ['rdoOrg']
         self.br.submit()
 
         self.br.select_form(predicate=select_form)
-        self.br.form['ctl00$cphMainContent$ucUnifiedSearch$txtFirm'] = q
+        self.br.form['ctl00$cphMainContent$ucUnifiedSearch$txtFirm'] = q.encode('utf8')
         self.br.submit()
 
         s = BeautifulSoup(self.br.response().read())
         r = re.compile(r'^ctl\d+_cphMainContent_grOrgResults$')
         t = s.find('table', id=r)
 
-        if not t:
+        if not t: # Not found
             print 'Not Found'
-            return # No results
+            firm.checked = True
+            firm.save()
+            return 
 
         tr = t.findAll('tr', recursive=False)
         tr = tr[2:-1] # Skip records-per-page header/footer and title header
 
         for row in tr:
-            td = row.findAll('td')
-            
-            print td[0].b.text.strip()
-            print td[1].text.strip()
-            print td[2].text.strip()
-            print td[3].text.strip()
+            td = row.findAll('td', recursive=False)
+
+            firm.legal_name = td[0].b.text.strip()
+            firm.other_name = td[1].text.strip()
+            firm.sec_number = td[2].text.strip()
+            firm.address = td[3].text.strip()
+            firm.checked = True
+            firm.save()
 
 if __name__ == '__main__':
     scraper = IapdScraper()
